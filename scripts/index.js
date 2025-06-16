6a82e93585346bdc4d2ee0271757f4231a47ebab
Tasyaa66 <natasyaazizahrahmawati@gmail.com>
2025-06-16 14:33:19 +0000

    Initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -9,14 +9,14 @@ function validateForm() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  //validasi username dan password, username dan password bisa diganti sesuai keinginan
+  //validasi usernamkeinginaninaninaninaninaninaniordan, username dan password bisa diganti sesuai keinginan
   if (username === "user" && password === "user") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
       text: "Selamat datang ayanggg",
       showConfirmButton: false,
-      timer: 1500,
+      ti mer: 1500,
     }).then(function () {
       window.location.href = "birthday.html";
     });
